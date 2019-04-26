e700cce07ff3adbb996a158603a6c7a291be57b5
anthopark <anthopark2015@gmail.com>
2019-04-26 16:34:09 -0700

    uploading page finished and local directory set for uploaded imgs

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, request, redirect
+from werkzeug import secure_filename
 import jinja2
 import os
 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_PATH = './static/uploaded_imgs/'
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return render_template("index.html")
+app.config['UPLOAD_FOLDER'] = UPLOAD_PATH
 
-@app.route('/change')
-def chance():
-	return redirect('/')
+@app.route('/')
+def display_main_page():
+	return render_template("upload_file.html")
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		uploaded_file = request.files['file']
+		uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'],
+										secure_filename(uploaded_file.filename)))
+	return render_template('result.html')
 
 @app.route('/post', methods=['GET','POST'])
 def post():
